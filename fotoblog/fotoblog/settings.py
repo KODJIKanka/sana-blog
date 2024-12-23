d7e30974cc3ec01ce6a372bedf86e5441b58e5f9
kkanka_sana <115672710+KODJIKanka@users.noreply.github.com>
2024-12-23 19:22:35 +0100

    Update settings.py

--- fotoblog/fotoblog/settings.py
+++ fotoblog/fotoblog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6+nmjs!w!!58-rjc+z30dvdevh156(^3g5$p_ib5l2u@d37c7j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
